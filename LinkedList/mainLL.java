d28134422677be960f9f891c38a843cb3c8a4885
Shirsty10 <shirsty1999@gmail.com>
2022-06-30 22:49:10 +0530

    added some more ll problems

--- LinkedList/mainLL.java
+++ LinkedList/mainLL.java
@@ -5,12 +5,12 @@ public class mainLL {
     public static void main(String[] args){
         Scanner inp = new Scanner(System.in);
         int n = inp.nextInt();
-        LinkedListImpl l1= new LinkedListImpl();
-        LinkedListImpl l2= new LinkedListImpl();
-        for(int i=0;i<=n;i++)
-           l1.insertAtStart(i);
-           for(int i=6;i<=10;i++)
-           l2.insertAtStart(i);
+         LinkedListImpl l1= new LinkedListImpl();
+         LinkedListImpl l2= new LinkedListImpl();
+        // for(int i=0;i<=n;i++)
+        //    l1.insertAtStart(i);
+        //    for(int i=6;i<=10;i++)
+        //    l2.insertAtStart(i);
         //  l1.traverse();
         // for(int i=0;i<=n;i++)
         //   l1.insertAtEnd(i);
@@ -35,19 +35,41 @@ public class mainLL {
     //  System.out.println();
     // System.out.println(l1.getKthElementFromEnd(8));
 
-    System.out.print("First List---- ");
-    l1.traverseUsingRecursion(l1.head);
-    System.out.println();
-    System.out.print("Second List---- ");
-    l1.traverseUsingRecursion(l2.head);
-    System.out.println();
+//     System.out.print("First List---- ");
+//     l1.traverseUsingRecursion(l1.head);
+//     System.out.println();
+//     System.out.print("Second List---- ");
+//     l1.traverseUsingRecursion(l2.head);
+//     System.out.println();
 
     
-utilityFunctions fun = new utilityFunctions();
-LinkedListImpl.Node newStart = fun.merge2LL(l1.head,l2.head);
+// utilityFunctions fun = new utilityFunctions();
+// LinkedListImpl.Node newStart = fun.merge2LL(l1.head,l2.head);
 
-l1.traverseUsingRecursion(newStart);
+// l1.traverseUsingRecursion(newStart);
+// LinkedListImpl.Node node=l1.reverseUsingPoiner(l1.head);
+// l1.traverseUsingRecursion(node);
+for(int i=0;i<n;i++){
+    int num = inp.nextInt();
+    l1.insertAtEnd(num);
+}
+System.out.println("Enter size for 2nd String : ");
+int n2 = inp.nextInt();
+for(int i=0;i<n2;i++){
+    int num = inp.nextInt();
+    l2.insertAtEnd(num);
+}
 
-      
+
+
+
+// int x = inp.nextInt();
+ utilityFunctions fun = new utilityFunctions();
+// LinkedListImpl.Node temp = fun.partition(l1.head, x);
+// l1.traverseUsingRecursion(temp);
+
+//System.out.println(l1.isLLPalindrome(l1.head));
+//System.out.println(l1.sizeLL() + " " + l2.sizeLL());
+      System.out.println(fun.insersectionPt(l1, l2));
     }
 }
--- LinkedList/utilityFunctions.java
+++ LinkedList/utilityFunctions.java
@@ -1,4 +1,5 @@
 
+
 public class utilityFunctions {
     public LinkedListImpl.Node merge2LL(LinkedListImpl.Node head,LinkedListImpl.Node head2){
         if(head==null)
@@ -15,8 +16,67 @@ public class utilityFunctions {
         return head;
     }
 
-    public LinkedListImpl.Node removeDuplicates(LinkedListImpl.Node head){
-        LinkedListImpl.Node newNode =null;
-        return newNode;
-    }
+    // public LinkedListImpl.Node removeDuplicates(LinkedListImpl.Node head){
+    //     LinkedListImpl l = new LinkedListImpl();
+    //     LinkedListImpl.Node  temp = head;
+    //     l.insertAtEnd(head.data);
+    //     while(temp!=null){
+    //         if(temp.data == l)
+    //     }
+    //     return newNode;
+    // }
+
+    /*Given the head of a linked list and a value x, partition it such that all nodes less than x come before nodes greater than or equal to x.
+      You should preserve the original relative order of the nodes in each of the two partitions.*/
+      public LinkedListImpl.Node partition(LinkedListImpl.Node head, int x){
+       
+        LinkedListImpl.Node smallHead = new LinkedListImpl.Node(-1);
+        LinkedListImpl.Node largeHead = new LinkedListImpl.Node(-1);
+        LinkedListImpl.Node small = smallHead;
+        LinkedListImpl.Node large = largeHead;
+        LinkedListImpl.Node temp = head;
+
+        while(temp!= null){
+            if(temp.data < x){
+               small.next =temp;
+               small = small.next;
+            }else{
+                large.next =temp;
+               large = large.next;
+            }
+            temp=temp.next;
+        }
+        small.next = null;
+        large.next = null;
+        small.next = largeHead.next;
+        return smallHead.next;
+
+
+      }
+
+      public int insersectionPt(LinkedListImpl head1,LinkedListImpl head2){
+          if(head1==null || head2 == null){
+              return 0;
+          }
+        LinkedListImpl.Node t1 = head1.head;
+        LinkedListImpl.Node t2 = head2.head;
+
+
+          int delta = Math.abs(head1.sizeLL()-head2.sizeLL());
+          //System.out.println(delta);
+          if(head1.sizeLL()>head2.sizeLL()){
+              for(int i=0;i<delta ;i++){
+                  t1 = t1.next;
+              }
+          }else if(head1.sizeLL()<head2.sizeLL()){
+            for(int i=0;i<delta ;i++){
+                t2 = t2.next;
+            }
+        }
+        while(t1.data!=t2.data){
+            t1=t1.next;
+            t2= t2.next;
+        }
+        return t1.data;
+      }
 }
